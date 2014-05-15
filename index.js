2f65ed0a94b54f7ac83ca64f26e6d3d7875c22eb
wearegrove <chris@wearegrove.co.uk>
2014-05-15 14:21:08 +0100

    it's alive

---
+++ index.js
@@ -0,0 +1,19 @@
+var path = require('path');
+
+var pattern = function(file) {
+  return {
+    pattern: file,
+    included: true,
+    served: true,
+    watched: false
+  };
+};
+
+var framework = function(files) {
+  files.unshift( pattern( path.join(__dirname, 'lib/jasmine-given.js' ) ) );
+};
+
+framework.$inject = ['config.files'];
+module.exports = {
+  'framework:jasmine-given': ['factory', framework]
+};
