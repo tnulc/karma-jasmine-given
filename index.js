d67357c08d39b7c6a5676613723570bcaf97690f
wearegrove <chris@wearegrove.co.uk>
2014-05-15 17:27:23 +0100

    now resolving jasmine-given as a dependency

--- index.js
+++ index.js
@@ -2,18 +2,16 @@ var path = require('path');
 
 var pattern = function(file) {
   return {
-    pattern: file,
-    included: true,
-    served: true,
-    watched: false
+    pattern: file, included: true, served: true, watched: false
   };
 };
 
 var framework = function(files) {
-  files.unshift( pattern( path.join(__dirname, 'lib/jasmine-given.js' ) ) );
+  files.unshift(pattern(path.dirname(require.resolve('jasmine-given')  + '/dist/jasmine-given.js')));
 };
 
 framework.$inject = ['config.files'];
+
 module.exports = {
   'framework:jasmine-given': ['factory', framework]
 };
